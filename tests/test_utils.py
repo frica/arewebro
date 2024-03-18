3de7c15adaa2765ea25329325ef41e5a594a31a9
Fabien Rica <fabien.rica@gmail.com>
2024-03-18 20:18:57 +0100

    import changed for pytest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,25 +1,25 @@
-import utils
-
+from src.utils import are_we_bro
+# have to import like this otherwise pytest plays dumb
 
 def test_arewebro_not_bros():
     """ test negative scenario """
-    assert utils.are_we_bro("Fabien", "Dark Vador") is False
+    assert are_we_bro("Fabien", "Dark Vador") is False
 
 
 def test_arewebro_empty():
     """ test empty strings """
-    assert utils.are_we_bro("", "") is False
+    assert are_we_bro("", "") is False
 
 
 def test_arewebro_empty_friend():
-    assert utils.are_we_bro("Fabien", "") is False
+    assert are_we_bro("Fabien", "") is False
 
 
 def test_arewebro_caps():
     """ test case """
-    assert utils.are_we_bro("Fabien", "PIERRE") is False
+    assert are_we_bro("Fabien", "PIERRE") is False
 
 
 def test_arewebro_bros():
     """ test positive scenario """
-    assert utils.are_we_bro("Fabien", "Pierre") is True
+    assert are_we_bro("Fabien", "Pierre") is True
